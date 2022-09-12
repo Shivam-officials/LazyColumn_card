5bf3e03e0853c12ef953d47f4137f2438315a73d
Shivam-officials <suryabhardwajofficial@gmail.com>
2022-09-12 06:20:56 +0530

    initiallise the basics codelab project and greetings and Myapp and preview of Myapp function

---
+++ app/src/main/java/com/example/basiccodelab/MainActivity.kt
@@ -0,0 +1,77 @@
+package com.example.basiccodelab
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedButton
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.basiccodelab.ui.theme.BasicCodelabTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            BasicCodelabTheme {
+                MyApp()
+            }
+        }
+    }
+}
+
+@Composable
+fun MyApp(names: List<String> = listOf("World", "Compose")) {
+    Surface(color = MaterialTheme.colors.background) {
+        Column(modifier = Modifier.padding(vertical = 4.dp)) {
+            for (name in names) {
+                Greeting(name = name)
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Surface(
+        color = MaterialTheme.colors.primary,
+        modifier = Modifier.padding(horizontal = 8.dp, vertical = 4.dp)
+    ) {
+        Row(modifier = Modifier.padding(24.dp)) {
+
+            //column for greetings
+            Column(
+                modifier = Modifier.weight(1f)
+            ) {
+                Text(text = "Hello")
+                Text(text = name)
+            }
+
+            // Outlined-button for showing more text
+            OutlinedButton(
+                onClick = { /*TODO*/ }
+            ) {
+                Text("Show more")
+            }
+
+
+        }
+
+
+    }
+}
+
+@Preview(showBackground = true, name = "Text Preview", widthDp = 320)
+@Composable
+fun DefaultPreview() {
+    BasicCodelabTheme {
+        MyApp()
+    }
+}
